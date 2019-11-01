0477b6f4817932d02f0a34aa1b108fec498e408a
Ani Sultanyan <Ani.Sultanyan@GMR.local>
2019-11-01 16:36:22 -0700

    added ReHash function

--- HashMapV2/HashMap.cs
+++ HashMapV2/HashMap.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Text;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Numerics;
 
 namespace HashMapV2
 {
@@ -11,18 +13,49 @@ namespace HashMapV2
     {
         LinkedList<Pair<TKey, TValue>>[] Collection;
         public int Count => Collection.Length;
-        public int NumberOfPairs => Collection.Select(x => x.Count).ToList().Count;
+        //public int NumberOfPairs => Collection.Where(x => x != null).Select(x => x.Count).ToList().Count; -> works but takes too long. used an int instead.
 
+        public int NumberOfPairs { get; private set; }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+               if(GetPair(key) == null)
+                {
+                    throw new Exception("key does not exist");
+                }
+
+                return GetPair(key).Value;
+            }
+
+            set
+            {
+                
+                if (GetPair(key) == null)
+                {
+                    Add(key, value);
+                }
+                else
+                {
+                    GetPair(key).Value = value;
+                }
+             
+            }
+        }
+            
         public HashMap()
         {
             Collection = new LinkedList<Pair<TKey, TValue>>[10];
+            NumberOfPairs = 0;
         }
+
         /// <summary>
         /// takes in a string and returns the hash value 
         /// </summary>
         /// <param name="input">string to hash</param>
         /// <returns>hash value</returns>
-        public int Hash(string input)
+        public int Hash(string input, int capacity)
         {
             int primeNum = 97;
 
@@ -33,7 +66,7 @@ namespace HashMapV2
                 hash += primeNum + input[i] + i;
             }
 
-            return hash % Collection.Length;
+            return hash % capacity;
         }
 
         public void Add(TKey key, TValue value)
@@ -43,7 +76,7 @@ namespace HashMapV2
 
         public void Add(Pair<TKey, TValue> pair)
         {
-            int index = Hash(pair.Key.ToString());
+            int index = Hash(pair.Key.ToString(), Count);
             if (Collection[index] == null)
             {
                 Collection[index] = new LinkedList<Pair<TKey, TValue>>();
@@ -70,15 +103,89 @@ namespace HashMapV2
                 }
 
             }
+
+            NumberOfPairs++;
+            if (NumberOfPairs > Collection.Length)
+            {
+                ReHash();
+            }
         }
 
-        //bool Remove(TKey key, TValue value)
-        //{
-        //    int index = Hash(key.ToString());
 
-            
-           
-        //}
+        public bool Remove(TKey key)
+        {
+            int index = Hash(key.ToString(), Count);
 
+            foreach (var item in Collection[index])
+            {
+                if (item.Key.Equals(key))
+                {
+                    Collection[index].Remove(item);
+                    NumberOfPairs--;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public void ReHash()
+        {
+            LinkedList<Pair<TKey, TValue>>[] temp = new LinkedList<Pair<TKey, TValue>>[Collection.Length * 2];
+
+            //for (int i = 0; i < Collection.Length; i++)
+            //{
+            //    foreach (var item in Collection[i])
+            //    {
+            //        int index = Hash(item.Key.ToString(), temp.Length);
+
+            //    }
+            //}
+
+
+            foreach (var list in Collection)
+            {
+                if (list != null)
+                {
+                    foreach (var kvp in list)
+                    {
+                        if (kvp != null)
+                        {
+                            int newbucket = Hash(kvp.Key.ToString(), temp.Length);
+
+                            if (temp[newbucket] == null)
+                            {
+                                temp[newbucket] = new LinkedList<Pair<TKey, TValue>>();
+                            }
+
+                            temp[newbucket].AddLast(kvp);
+
+                        }
+                    }
+                }
+            }
+
+            Collection = temp;
+
+
+        }
+
+        Pair<TKey,TValue> GetPair(TKey key)
+        {
+            int index = Hash(key.ToString(), Count);
+
+        
+                foreach(var item in Collection[index])
+                {
+                    if(item.Key.Equals(key))
+                    {
+                        return item;
+                    }
+                }
+
+            return null;
+
+        }
     }
 }
--- HashMapV2/Program.cs
+++ HashMapV2/Program.cs
@@ -5,15 +5,38 @@ namespace HashMapV2
 
     class Program
     {
-  
+
         static void Main(string[] args)
         {
             HashMap<int, string> map = new HashMap<int, string>();
 
-            map.Add(1, "uhoh");
-            map.Add(2, "stinky");
-            map.Add(3, "poopy");
-            map.Add(4, "hehe");
+            //map.Add(19, "poopy");
+            //map.Add(29, "stinky");
+            //Console.WriteLine("Enter a key:");
+            //int key = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter a value:");
+            //string value = Console.ReadLine();
+            //map.Add(key, value);
+
+            //Console.WriteLine(map.NumberOfPairs);
+
+            //Console.WriteLine("Enter a key to remove:");
+            //key = int.Parse(Console.ReadLine());
+
+            //map.Remove(key);
+
+            string a = "a";
+            for (int i = 0; i < 10; i++)
+            {
+                map.Add(i, a);
+
+                a += "a";
+            }
+
+            ;
+            map.Add(69, "poopie");
+
+
 
             Console.WriteLine(map.NumberOfPairs);
 
